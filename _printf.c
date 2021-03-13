c4fe0f50b61657f7257bbfa11fe0744a8ccf1e92
alaedine07 <alaedineboufaden@gmail.com>
2021-03-13 17:34:00 +0100

    added manpage and conversion files

--- _printf.c
+++ _printf.c
@@ -1,26 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
 /**
 *
 *
 */
 int _printf(const char *format, ...)
 {
-//if format not exit --> return -1
-// else call parser
-int i;
-va_list arguments;
-if (format[i] == NULL)
-{
-	return (-1);
-}
-else
-{
-	parser(format);
-}
-va_start(arguments, format);
+	//if format not exit --> return -1
+	// else call parser
+	int i;
+	va_list arguments;
+	if (format[i] == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		parser(format);
+	}
+	va_start(arguments, format);
 
 
 
 
-va_end(arguments);
+	va_end(arguments);
 
 }
