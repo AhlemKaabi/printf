e448f6389c15d1e15232070e5c82c4fcaec1cd18
alaedine07 <alaedineboufaden@gmail.com>
2021-03-13 18:34:32 +0100

    comments explaining

--- _printf.c
+++ _printf.c
@@ -9,6 +9,7 @@ int _printf(const char *format, ...)
 	//if format not exit --> return -1
 	// else call parser
 	int i;
+	void (*fun_ptr)(va_list);
 	va_list arguments;
 	if (format[i] == NULL)
 	{
@@ -16,9 +17,12 @@ int _printf(const char *format, ...)
 	}
 	else
 	{
-		parser(format);
+		va_start(arguments, format);
+		fun_ptr = *parser(format);
+		/* if fun_ptr is NULL print the format */
+		/* if fun_ptr is not NULL execute it with fun_ptr(argument) */
 	}
-	va_start(arguments, format);
+	
 
 
 
