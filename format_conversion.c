59f9550a4e1ecc5acd64f7d5833d2922d92d4bda
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-14 23:34:38 +0100

    addiing print_int function

--- format_conversion.c
+++ format_conversion.c
@@ -31,3 +31,42 @@ int print_string(va_list arg)
 	}
 	return (i);
 }
+/**
+* print_int - function that prints integers
+* @arg: argument of type va_list
+* @return: number of elements printed
+*/
+int print_int(va_list arg)
+{
+	int n = va_arg(arg, int);
+	int i, num, div, o, Count = 0;
+
+	o = n % 10;
+	n = n / 10;
+	if (n < 0)
+	{
+		n = -n;
+		o = -o;
+		_putchar('-');
+	}
+	num = n;
+	div = 1;
+	if (num > 0)
+	{
+		while ((num / 10) != 0)
+		{
+			num = num / 10;
+			div = div * 10;
+		}
+		while (div >= 1)
+		{
+			i = n / div;
+			_putchar(i + '0');
+			n = n % div;
+			div = div / 10;
+			Count++;
+		}
+	}
+	_putchar(o + '0');
+	return (Count);
+}
