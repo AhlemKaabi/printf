073f92e46767f3338195a0bbc3ccb1fc34539dc7
alaedine07 <alaedineboufaden@gmail.com>
2021-03-15 00:23:17 +0100

    removed parser.c

--- parser.c
+++
@@ -1,7 +0,0 @@
-int (*parser(char *format))
-{
-	/* declare table of type match_conversion */
-	/* loop through the format and check for % */
-	/* if no % is found return NULL */
-	/* if a match is found return a pointer to the matched function */
-}
