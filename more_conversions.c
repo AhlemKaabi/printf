05c0df527f79fce652a420de826a077a6246f331
alaedine07 <alaedineboufaden@gmail.com>
2021-03-16 23:41:24 +0100

    print rev with ptr and malloc

--- more_conversions.c
+++ more_conversions.c
@@ -81,12 +81,19 @@ char *rev_string(char *s)
 	_memcpy(dest, s, len);
 	for (start = 0; start < len; start++, len--)
 	{
-	 	aux = dest[len - 1];
+		aux = dest[len - 1];
 		dest[len - 1] = dest[start];
 		dest[start] = aux;
 	}
 	return (dest);
 }
+/**
+ * _memcpy - copy memory area
+ * @dest: Destination for copying
+ * @src: Source to copy from
+ * @n: The number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
